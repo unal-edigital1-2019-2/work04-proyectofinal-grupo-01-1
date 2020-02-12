2384e0ddd0ccf95f688a404a30c117a628e14b83
julian <37085956+jdpulidoca@users.noreply.github.com>
2020-02-12 23:54:52 +0100

    Barra de Colores Corrida

---
+++ hdl/_impact.cmd
@@ -0,0 +1,34 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+Identify -inferir 
+identifyMPM 
+assignFile -p 1 -file "C:/Users/Julian Pulido/Documents/GitHub/work04-proyectofinal-grupo-01-1/hdl/test_cam.bit"
+Program -p 1 
+Program -p 1 
+assignFile -p 1 -file "C:/Users/Julian Pulido/Documents/GitHub/work04-proyectofinal-grupo-01-1/hdl/test_cam.bit"
+Program -p 1 
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "\auto_project.ipf"
+setMode -bs
+setMode -bs
+deleteDevice -position 1
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
--- hdl/src/cam_read.v
+++ hdl/src/cam_read.v
@@ -41,51 +41,45 @@ captura de datos de la camara
 debe tener en cuenta el nombre de las entradas  y salidad propuestas 
 
 ********************************************************************************/
-	 reg contador=0;
-	 reg [14:0]addr=0;
 	 reg [1:0]cs=0;
 	 reg ovsync;
-
+	 
 always @ (posedge pclk) begin
 	case (cs)
 	0: begin
 		if(ovsync && !vsync)begin
-			cs=1;
+		cs=1;
+		mem_px_addr=0;
 		end
-		addr=0;
-		mem_px_addr<=0;
+		mem_px_addr=0;
 	end
 	1: begin
+		px_wr=0;
 		if (href) begin
-			cs=2;
-				if	(contador==0)begin
-				px_wr<=0;
-				mem_px_data[7]<=px_data[7];
-				mem_px_data[6]<=px_data[6];
-				mem_px_data[5]<=px_data[5];
-				mem_px_data[4]<=px_data[2];
-				mem_px_data[3]<=px_data[1];
-				mem_px_data[2]<=px_data[0];
-				contador=contador+1;
-			end
-		end
-		if (vsync && !ovsync) begin
-			cs=0;
+				mem_px_data[7]=px_data[7];
+				mem_px_data[6]=px_data[6];
+				mem_px_data[5]=px_data[5];
+				mem_px_data[4]=px_data[2];
+				mem_px_data[3]=px_data[1];
+				mem_px_data[2]=px_data[0];
+				cs=2;
 		end
 	end
 	2: begin
-			if	(contador==1)begin
-				mem_px_data[1]<=px_data[4];
-				mem_px_data[0]<=px_data[3];
-				mem_px_addr<=addr;
-				addr=addr+1;
+				mem_px_data[1]=px_data[4];
+				mem_px_data[0]=px_data[3];
+			 	px_wr=#1 1;
+				mem_px_addr=mem_px_addr+1;
 				cs=1;
-				contador=contador+1;
-				#1
-				px_wr<=1;
-			end
+		if(vsync) begin
+		cs=0;
+		end		
+		if (mem_px_addr==19200) begin
+			mem_px_addr=0;
+			cs=0;
+		end
 		end
 endcase
-	ovsync=vsync;
+	ovsync<=vsync;
 end
 endmodule
